a7d1f2d816a01f07a2377888414db3641aa0379d
ankanuj <s.singhanuj132@gmail.com>
2020-09-11 01:57:52 +0530

    heroku

--- blog_poet/settings.py
+++ blog_poet/settings.py
@@ -147,8 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog_poet/static'),]
 
 
 #media folder settings
