197341c858ecdffc38bdba58c0275ee2e8665d31
ankanuj <s.singhanuj132@gmail.com>
2020-09-11 00:58:30 +0530

    heroku

--- blog_poet/settings.py
+++ blog_poet/settings.py
@@ -144,11 +144,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS =[ 
-    os.path.join(BASE_DIR,'blog_poet/static'),
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 #media folder settings
